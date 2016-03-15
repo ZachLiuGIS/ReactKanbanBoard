8733b52f0522d90edc1d77ebce5ee1a45722b4de
ZachLiuGIS <zachliugis@gmail.com>
2016-03-15 10:37:59 -0400

    added animation and drag and drop.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,9 @@
 import React, { Component, PropTypes} from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import CheckList from './CheckList';
 import marked from 'marked';
-
+import { DragSource, DropTarget } from 'react-dnd';
+import constants from '../constants/constants';
 
 let titlePropType = (props, propName, componentName) => {
     if (props[propName]) {
@@ -11,6 +13,38 @@ let titlePropType = (props, propName, componentName) => {
                 `${propName} in ${componentName} is longer than 80 characters`
             );
         }
+    }
+};
+
+const cardDragSpec = {
+    beginDrag(props) {
+        return {
+            id: props.id,
+            status: props.status
+        };
+    },
+
+    endDrag(props) {
+        props.cardCallbacks.persistCardDrag(props.id, props.status);
+    }
+};
+
+const cardDropSpec = {
+    hover(props, monitor) {
+        const draggedId = monitor.getItem().id;
+        props.cardCallbacks.updatePosition(draggedId, props.id);
+    }
+};
+
+let collectDrag = (connect, monitor) => {
+    return {
+        connectDragSource: connect.dragSource()
+    };
+};
+
+let collectDrop = (connect, monitor) => {
+    return {
+        connectDropTarget: connect.dropTarget()
     }
 };
 
@@ -30,6 +64,7 @@ class Card extends Component {
     }
 
     render() {
+        const { connectDragSource, connectDropTarget } = this.props;
         let cardDetails;
 
         if (this.state.showDetails) {
@@ -53,16 +88,19 @@ class Card extends Component {
             backgroundColor: this.props.color
         };
 
-        return (
+        return connectDropTarget(connectDragSource(
             <div className="card">
                 <div style={sideColor}/>
                 <div className={this.state.showDetails? "card_title card_title--is-open" : "card_title"}
                      onClick={this.toggleDetails.bind(this)}>{this.props.title}
                 </div>
-                {cardDetails}
-
+                <ReactCSSTransitionGroup transitionName="toggle"
+                                         transitionEnterTimeout={250}
+                                         transitionLeaveTimeout={250} >
+                    {cardDetails}
+                </ReactCSSTransitionGroup>
             </div>
-        )
+        ));
     }
 }
 
@@ -72,8 +110,12 @@ Card.propTypes = {
     description: PropTypes.string,
     color: PropTypes.string,
     tasks: PropTypes.arrayOf(PropTypes.object),
-    taskCallbacks: PropTypes.object
-}
-;
+    taskCallbacks: PropTypes.object,
+    cardCallbacks: PropTypes.object,
+    connectDragSource: PropTypes.func.isRequired,
+    connectDropTarget: PropTypes.func.isRequired
+};
 
-export default Card;
+const dragHighOrderCard = DragSource(constants.CARD, cardDragSpec, collectDrag)(Card);
+const dragDropHighOrderCard = DropTarget(constants.CARD, cardDropSpec, collectDrop)(dragHighOrderCard);
+export default dragDropHighOrderCard;
--- src/components/List.js
+++ src/components/List.js
@@ -1,27 +1,47 @@
 import React, { Component, PropTypes} from 'react';
+import { DropTarget } from 'react-dnd';
 import Card from './Card';
+import constants from '../constants/constants';
+
+const listTargetSpec = {
+    hover(props, monitor) {
+        const draggedId = monitor.getItem().id;
+        props.cardCallbacks.updateStatus(draggedId, props.id)
+    }
+};
+
+let collect = (connect, monitor) => {
+    return {
+        connectDropTarget: connect.dropTarget()
+    };
+};
 
 class List extends Component {
     render() {
+        const { connectDropTarget } = this.props;
+
         var cards = this.props.cards.map((card) => {
             return <Card taskCallbacks={this.props.taskCallbacks}
+                         cardCallbacks={this.props.cardCallbacks}
                          key={card.id}
                          {...card} />
         });
 
-        return (
+        return connectDropTarget(
             <div className="list">
                 <h1>{this.props.title}</h1>
                 {cards}
             </div>
-        )
+        );
     }
 }
 
 List.propTypes = {
     title: PropTypes.string.isRequired,
     cards: PropTypes.arrayOf(PropTypes.object),
-    taskCallbacks: PropTypes.object
+    taskCallbacks: PropTypes.object,
+    cardCallbacks: PropTypes.object,
+    connectDropTarget: PropTypes.func.isRequired
 };
 
-export default List;
+export default DropTarget(constants.CARD, listTargetSpec, collect)(List);
